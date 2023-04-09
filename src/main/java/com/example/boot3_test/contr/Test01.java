410d11a4c77ec2d1191927b0bd06f994937c2bf4
xiaoyu <1556818269@qq.com>
2023-04-09 12:16:05 +0800

    修改3

---
+++ src/main/java/com/example/boot3_test/contr/Test01.java
@@ -0,0 +1,13 @@
+package com.example.boot3_test.contr;
+
+/**
+ * @Author yujie
+ * @Description
+ * @Date Created in 2023-04-09 12:10
+ * @Version 1.0
+ */
+public class Test01 {
+    public static void main(String[] args) {
+        System.out.println(666);
+    }
+}
